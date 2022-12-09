6b22b760f80b3142d014dcf9aed6648395a13aae
Nathan Silverman <nzsilverman@gmail.com>
2022-12-09 11:28:28 -0700

    Day 9 revised
    A slightly cleaner solution after solving part 2

--- 2022/day9.py
+++ 2022/day9.py
@@ -8,41 +8,7 @@ def readInput(filename):
         lines = [line.strip() for line in f]
     return lines
 
-def simulateMovement(hLoc, tLoc, direction, steps):
-    tailMovement = set()
-    for step in range(steps):
-        # Update hLoc
-        if direction == 'U':
-            hLoc.y += 1
-        elif direction == 'D':
-            hLoc.y -= 1
-        elif direction == 'R':
-            hLoc.x += 1
-        elif direction == 'L':
-            hLoc.x -= 1
-
-
-        # Update tLoc
-        if (hLoc.x != tLoc.x) and (hLoc.y != tLoc.y): # H moved diagonally
-            signX = 1 if (hLoc.x > tLoc.x) else -1
-            signY = 1 if (hLoc.y > tLoc.y) else -1
-            distance = 1 if abs(hLoc.x - tLoc.x) >=2 or abs(hLoc.y - tLoc.y) >= 2 else 0
-            tLoc.x += signX * distance
-            tLoc.y += signY * distance
-        else: # H moved within a row or column
-            if (hLoc.x - tLoc.x) >= 2:
-                tLoc.x += 1
-            elif (tLoc.x - hLoc.x) >= 2:
-                tLoc.x -= 1
-            elif (hLoc.y - tLoc.y) >= 2:
-                tLoc.y += 1
-            elif (tLoc.y - hLoc.y) >= 2:
-                tLoc.y -= 1
-
-        tailMovement.add((tLoc.x, tLoc.y))
-    return tailMovement
-
-def simulateStepHeadTail(hLoc, tLoc, direction, moveHead=False):
+def simulateStep(hLoc, tLoc, direction, moveHead=False):
     if moveHead:
         # Update hLoc
         if direction == 'U':
@@ -82,9 +48,9 @@ def part1(lines):
 
     for line in lines:
         direction, steps = line.split()
-        tailMovement = simulateMovement(hLoc, tLoc, direction, int(steps))
-        for item in tailMovement:
-            tVisitedLocs.add(item)
+        for step in range(int(steps)):
+            simulateStep(hLoc, tLoc, direction, moveHead=True)
+            tVisitedLocs.add((tLoc.x, tLoc.y))
 
     print(f"Size of tVisitedLocs set: {len(tVisitedLocs)}")
 
@@ -99,7 +65,7 @@ def part2(lines):
             for i in range(1,len(knotLocs)):
                 head = knotLocs[i-1]
                 tail = knotLocs[i]
-                simulateStepHeadTail(head, tail, direction, i==1)
+                simulateStep(head, tail, direction, i==1)
                 if i == 9:
                     tVisitedLocs.add((tail.x, tail.y))
     print(f"Size of tVisitedLocs set: {len(tVisitedLocs)}")
@@ -110,8 +76,8 @@ def main():
     # lines = readInput('example.txt')
     # lines = readInput('example2.txt')
     lines = readInput('input.txt')
-    # print("Part 1")
-    # part1(lines)
+    print("Part 1")
+    part1(lines)
     print("Part 2")
     part2(lines)
 
