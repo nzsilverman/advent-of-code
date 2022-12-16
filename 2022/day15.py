40b84ec6182f6728fb9aaaff4fb904e8100ec268
Nathan Silverman <nathan.silverman@flypyka.com>
2022-12-16 15:30:17 -0800

    Day 15 part 2 works

--- 2022/day15.py
+++ 2022/day15.py
@@ -1,5 +1,12 @@
 import sys
 from collections import deque
+# from matplotlib.patches import Polygon
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import shapely.geometry as sg
+from shapely.plotting import plot_polygon
+
 
 infile = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
 row = int(sys.argv[2]) if len(sys.argv)>2 else 2000000
@@ -45,35 +52,6 @@ def part1():
 
     print(f"Invalid locations in row {row}: {len(invalidLocations)}")
 
-# part1()
-
-# Works on example but is not optimized enough
-# def part2():
-#     if infile == 'input.txt':
-#         xMin, xMax = 0, 4000000
-#         yMin, yMax = 0, 4000000
-#     else:
-#         xMin, xMax = 0, 20
-#         yMin, yMax = 0, 20
-
-#     invalidLocations = set()
-#     for x in range(xMin, xMax + 1):
-#         for y in range(yMin, yMax + 1):
-#             print(f"Investigating (x,y): ({x},{y})")
-#             for sensor, beacon in zip(sensors, beacons):
-#                 # print(f"sensor: {sensor}, beacon: {beacon}")
-#                 sx, sy = sensor
-#                 bx, by = beacon
-#                 dist = manhattanDistance(sx, sy, bx, by)
-#                 pointToSensor = manhattanDistance(x, y, sx, sy)
-#                 if pointToSensor <= dist and (x, y) not in beacons and (x, y) not in sensors:
-#                     invalidLocations.add((x, y))
-
-#     for x in range(xMin, xMax + 1):
-#         for y in range(yMin, yMax + 1):
-#             if (x,y) not in invalidLocations and (x,y) not in beacons and (x,y) not in sensors:
-#                 print(f"({x}, {y}) not in invalid locations set. Tuning frequency: {x*4000000 + y}")
-
 def part2():
     if infile == 'input.txt':
         xMin, xMax = 0, 4000000
@@ -82,8 +60,35 @@ def part2():
         xMin, xMax = 0, 20
         yMin, yMax = 0, 20
 
-    for x in range(xMin, xMax + 1):
-        for y in range(yMin, yMax + 1):
-            continue
+    polygons = []
+    backgroundRect = sg.Polygon([[xMin, yMin], [xMin, yMax], [xMax, yMax], [xMax, yMin]])
 
+    fig, ax = plt.subplots()
+    for sensor, beacon in zip(sensors, beacons):
+        sx, sy = sensor
+        bx, by = beacon
+        dist = manhattanDistance(sx, sy, bx, by)
+        print(f"drawing sensor centered at ({sx}, {sy}) and with distance: {dist}")
+        points = np.array([(sx + dist,sy),(sx,sy+dist),(sx - dist,sy),(sx,sy-dist)])
+        p = mpl.patches.Polygon(points, fc="white", ec="black")
+        polygons.append(sg.Polygon(points))
+        ax.add_patch(p)
+
+    mergedPolygon = sg.Polygon()
+    for polygon in polygons:
+        mergedPolygon = mergedPolygon.union(polygon)
+
+    differenceRect = backgroundRect - mergedPolygon
+    differentPoint = differenceRect.centroid
+    tuningFrequency = differentPoint.x * 4000000 + differentPoint.y
+    print(f"tuningFrequency: {tuningFrequency}")
+
+    plot_polygon(differenceRect, ax=ax)
+
+    ax.set_xlim([xMin,xMax])
+    ax.set_ylim([yMin,yMax])
+    plt.show()
+
+
+part1()
 part2()
