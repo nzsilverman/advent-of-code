3ae5c301a6ac0930c86dc4c05e839ab7ce23d0b9
Nathan Silverman <nzsilverman@gmail.com>
2022-12-15 20:27:18 -0800

    day 15 part 1 works

--- 2022/day15.py
+++ 2022/day15.py
@@ -26,89 +26,64 @@ for line in lines:
     minX = min(min(int(sx), minX), int(bx))
     minY = min(min(int(sy), minY), int(by))
 
-print(f"maxX: {maxX}, maxY: {maxY}")
-
-# matrix = [['.' for x in range(maxX + 10)] for x in range(maxY + 10)]
-
-# for sensor in sensors:
-#     x, y = sensor
-#     matrix[y][x] = 'S'
-
-# for beacon in beacons:
-#     x, y = beacon
-#     matrix[y][x] = 'B'
-
-# def printMatrix():
-#     for line in matrix:
-#         print(line)
-
 def manhattanDistance(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
 
-# def addInvalidLocations(sensor, beacon):
-#     global matrix
-#     sx, sy = sensor
-#     bx, by = beacon
-#     dist = manhattanDistance(sx, sy, bx, by)
-#     print(f"distance: {dist}")
 
-#     for rowIdx, row in enumerate(matrix):
-#         for colIdx, val in enumerate(row):
-#             if manhattanDistance(colIdx, rowIdx, sx, sy) <= dist:
-#                 if matrix[rowIdx][colIdx] == '.':
-#                     matrix[rowIdx][colIdx] = "#"
-
-invalidLocations = set()
-
-# def addInvalidLocations(sensor, beacon):
-#     sx, sy = sensor
-#     bx, by = beacon
-#     dist = manhattanDistance(sx, sy, bx, by)
-#     for a in range(dist + 1):
-#         b = abs(dist - a) + 1
-#         for i in range(b):
-#             x1 = sx + a
-#             y1 = sy + b
-#             x2 = sx - a
-#             y2 = sy - b
-#             invalidLocations.add((x1, y1))
-#             invalidLocations.add((x2, y2))
-
-
-# for sensor, beacon in zip(sensors, beacons):
-#     print(f"sensor: {sensor}, beacon: {beacon}")
-#     addInvalidLocations(sensor, beacon)
-
-# invalidLocations = set()
-
-# for i in range(minX-100000, maxX+100000):
-#     x, y = i, row
-#     for sensor, beacon in zip(sensors, beacons):
-#         sx, sy = sensor
-#         bx, by = beacon
-#         dist = manhattanDistance(sx, sy, bx, by)
-#         if manhattanDistance(sx, sy, x, y) <= dist:
-#             if (x,y) not in beacons and (x,y) not in sensors:
-#                 invalidLocations.add((x, y))
-
-for x in range(minX, maxX + 1):
+def part1():
+    invalidLocations = set()
     for sensor, beacon in zip(sensors, beacons):
         print(f"sensor: {sensor}, beacon: {beacon}")
         sx, sy = sensor
         bx, by = beacon
         dist = manhattanDistance(sx, sy, bx, by)
-        pointToSensor = manhattanDistance(x, row, sx, sy)
-        if pointToSensor <= dist and (x, row) not in beacons and (x, row) not in sensors:
-            invalidLocations.add((x, row))
+        offset = 1_000_000
+        for x in range(minX-offset, maxX + offset):
+            pointToSensor = manhattanDistance(x, row, sx, sy)
+            if pointToSensor <= dist and (x, row) not in beacons and (x, row) not in sensors:
+                invalidLocations.add((x, row))
 
+    print(f"Invalid locations in row {row}: {len(invalidLocations)}")
 
+# part1()
 
-count = 0
-for item in invalidLocations:
-    x,y = item
-    if y == row:
-        print(item)
-        count += 1
+# Works on example but is not optimized enough
+# def part2():
+#     if infile == 'input.txt':
+#         xMin, xMax = 0, 4000000
+#         yMin, yMax = 0, 4000000
+#     else:
+#         xMin, xMax = 0, 20
+#         yMin, yMax = 0, 20
 
-print(f"Count: {count}")
+#     invalidLocations = set()
+#     for x in range(xMin, xMax + 1):
+#         for y in range(yMin, yMax + 1):
+#             print(f"Investigating (x,y): ({x},{y})")
+#             for sensor, beacon in zip(sensors, beacons):
+#                 # print(f"sensor: {sensor}, beacon: {beacon}")
+#                 sx, sy = sensor
+#                 bx, by = beacon
+#                 dist = manhattanDistance(sx, sy, bx, by)
+#                 pointToSensor = manhattanDistance(x, y, sx, sy)
+#                 if pointToSensor <= dist and (x, y) not in beacons and (x, y) not in sensors:
+#                     invalidLocations.add((x, y))
 
+#     for x in range(xMin, xMax + 1):
+#         for y in range(yMin, yMax + 1):
+#             if (x,y) not in invalidLocations and (x,y) not in beacons and (x,y) not in sensors:
+#                 print(f"({x}, {y}) not in invalid locations set. Tuning frequency: {x*4000000 + y}")
+
+def part2():
+    if infile == 'input.txt':
+        xMin, xMax = 0, 4000000
+        yMin, yMax = 0, 4000000
+    else:
+        xMin, xMax = 0, 20
+        yMin, yMax = 0, 20
+
+    for x in range(xMin, xMax + 1):
+        for y in range(yMin, yMax + 1):
+            continue
+
+part2()
