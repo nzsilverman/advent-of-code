d683f476dd90813cb2e1e2b30054bf6b56b839a1
Nathan Silverman <nzsilverman@gmail.com>
2022-12-18 11:07:05 -0800

    Day 17 part 1 works

--- 2022/day17.py
+++ 2022/day17.py
@@ -25,59 +25,32 @@ five =  [["#", "#"],
 
 rocks.extend([one, two, three, four, five])
 
-# Todo this is broken
-def checkIfRockHitsAnything(tower, rock, x, y, direction):
-    stopFound = False
+# rockLeftX is the location of the left side of the rock
+# rockBottomY is the location of the bottom of the rock
+def checkIfRockHitsAnything(tower, rock, rockLeftX, rockBottomY, direction):
+    # for colIdx in range(len(rock)-1, -1, -1):
     for colIdx in range(len(rock)):
+        towerY = rockBottomY + (len(rock) - colIdx - 1)
+
         for rowIdx in range(len(rock[0])):
-            print(f"colIdx: {colIdx}, rowIdx: {rowIdx}, y: {y}, x: {x}, len(tower): {len(tower)}")
-            relativeY = y - (len(rock) - colIdx - 1)
-            print(f"relativeY: {relativeY}")
-            # if y <= len(tower):
+            towerX = rockLeftX + rowIdx
             if direction == 'left':
-                if rowIdx + x - 1 < 0:
-                    stopFound = True
-                    break
+                if towerX - 1 < 0:
+                    return True
 
-                if relativeY >= len(tower):
-                    break
-                # print(f"tower[y][rowIdx - x -1]: {tower[y][rowIdx - x - 1]}")
-                # print(f"rock[colIdx][rowIdx]: {rock[colIdx][rowIdx]}")
-                if tower[relativeY][rowIdx + x - 1] in ['-', '#'] and rock[colIdx][rowIdx] == '#':
-                    stopFound = True
-                    break
+                if towerY < len(tower) and tower[towerY][towerX-1] == '#' and rock[colIdx][rowIdx] == '#':
+                    return True
             elif direction == 'right':
-                # if rowIdx + x + 1 < len(tower[0]) or y == len(tower):
-                # print(f"colIdx: {colIdx}, rowIdx: {rowIdx}, y: {y}, x: {x}, len(tower[0]): {len(tower[0])}, len(rock[0]): {len(rock[0])}")
-                if x + len(rock[0]) + 1 > len(tower[0]):
-                    stopFound = True
-                    break
+                if towerX + 1 >= len(tower[0]):
+                    return True
 
-                if relativeY >= len(tower):
-                    break
-
-                if tower[relativeY][rowIdx + x + 1] in ['-', '#'] and rock[colIdx][rowIdx] == '#':
-                    stopFound = True
-                    break
+                if towerY < len(tower) and tower[towerY][towerX+1] == '#' and rock[colIdx][rowIdx] == '#':
+                    return True
             elif direction == 'down':
-                if relativeY > len(tower):
-                    break
-
-                if tower[relativeY-1][rowIdx + x] in ['-', '#'] and rock[colIdx][rowIdx] == '#':
-                    print(f"tower line: ")
-                    for line in tower[relativeY-1:relativeY]:
-                        print(line)
-                    print(f"rock[colIdx]")
-                    print(rock[colIdx])
-                    print(f"Downward stop found: tower[relativeY-1][rowIdx+x]: {tower[relativeY-1][rowIdx+x]}, rock[colIdx][rowIdx]: {rock[colIdx][rowIdx]}")
-
-                    stopFound = True
-                    break
-
-        if stopFound:
-            break
-
-    return stopFound
+                if towerY <= len(tower):
+                    if tower[towerY-1][towerX] in ['-', '#'] and rock[colIdx][rowIdx] == '#':
+                        return True
+    return False
 
 def part1():
     numRocksToFall = 2022
@@ -106,38 +79,31 @@ def part1():
                 print("Skipping moving right")
 
             # Move down if possible, else come to rest
-            # stopFound = False
-            # for colIdx in range(len(rock)):
-            #     for rowIdx in range(len(rock[0])):
-            #         print(f"colIdx: {colIdx}, rowIdx: {rowIdx}, y: {y}, x: {x}, len(tower): {len(tower)}")
-            #         if y <= len(tower):
-            #             if tower[y-1][rowIdx + x] in ['-', '#'] and rock[colIdx][rowIdx] == '#':
-            #                 stopFound = True
-            #                 break
+            rockHitsSomething = checkIfRockHitsAnything(tower, rock, x, y, "down")
 
-            #     if stopFound:
-            #         break
-            stopFound = checkIfRockHitsAnything(tower, rock, x, y, "down")
-
-
-            if not stopFound:
+            if not rockHitsSomething:
                 print("Moving down")
                 y -= 1
             else:
                 print("End found")
-                for row in reversed(rock):
-                    tmp = ['.' for i in range(x)]
-                    tmp.extend(row)
-                    tmp.extend(['.' for i in range(towerWidth - len(row) - x)])
-                    tower.append(tmp)
+                for rowIdx, row in enumerate(reversed(rock)):
+                    if (y+rowIdx) < len(tower):
+                        for idx in range(len(row)):
+                            if row[idx] == '#':
+                                tower[y+rowIdx][idx + x] = '#'
+                    else:
+                        tmp = ['.' for i in range(x)]
+                        tmp.extend(row)
+                        tmp.extend(['.' for i in range(towerWidth - len(row) - x)])
+                        tower.append(tmp)
                 break
 
-        print(f"After rock {rockNum} fell, tower looks like:")
-        for line in reversed(tower):
-            print(line)
+        # print(f"After rock {rockNum} fell, tower looks like:")
+        # for line in reversed(tower):
+        #     print(line)
 
-        if rockNum > 2:
-            break
+        # if rockNum > 2:
+        #     break
 
     print(f"Height of fallen rocks: {len(tower) - 1}")
 
