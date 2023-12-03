e35209bb3201ebffde14ae4e2b09e8ec6d2af23e
Nathan Silverman <nzsilverman@gmail.com>
2023-12-03 10:43:30 -0800

    Some reworking of my template

--- 2023/template.py
+++ 2023/template.py
@@ -1,9 +1,23 @@
 import sys
 from collections import deque
 
-infile = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
-data = open(infile).read().strip()
-lines = [line for line in data.split('\n')]
+def PrintRed(skk, end="\n"): PrintDebug("\033[91m{}\033[00m".format(skk), end=end)
+def PrintGreen(skk, end="\n"): PrintDebug("\033[92m{}\033[00m".format(skk), end=end)
+def PrintYellow(skk, end="\n"): PrintDebug("\033[93m{}\033[00m".format(skk), end=end)
+def PrintLightPurple(skk, end="\n"): PrintDebug("\033[94m{}\033[00m".format(skk), end=end)
 
-for line in lines:
-    print(line)
+DEBUG=1
+def PrintDebug(skk, end="\n"):
+    if DEBUG:
+        print(skk, end=end)
+
+def main():
+    infile = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
+    data = open(infile).read().strip()
+    lines = [line for line in data.split('\n')]
+
+    for line in lines:
+        PrintDebug(line)
+
+if __name__ == '__main__':
+    main()
