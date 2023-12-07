0128ad4f63fe60ad81f999fb9afc76109ad72d71
Nathan Silverman <nzsilverman@gmail.com>
2023-12-07 08:38:23 -0800

    Building maps naively for part 5, too slow though

--- 2023/day5.py
+++ 2023/day5.py
@@ -30,13 +30,50 @@ def PrintDebug(skk, end="\n"):
 
 @attrs.define
 class Almanac():
-  seed_to_soil_map: dict
-  soil_to_fertilizer_map: dict
-  fertilizer_to_water_map: dict
-  water_to_light_map: dict
-  light_to_temperature_map: dict
-  temperature_to_humidity_map: dict
-  humidity_to_location_map: dict
+  seed_to_soil_map: dict = {}
+  soil_to_fertilizer_map: dict = {}
+  fertilizer_to_water_map: dict = {}
+  water_to_light_map: dict = {}
+  light_to_temperature_map: dict = {}
+  temperature_to_humidity_map: dict = {}
+  humidity_to_location_map: dict = {}
+
+
+def BuildAlmanac(lines):
+  chunks = [idx for idx, line in enumerate(lines) if line == ""]
+
+  assert len(chunks) == 7
+  almanac = Almanac()
+
+  # offset is 2 for the whitespace and title for each section
+  offset = 2
+  maps = [
+    almanac.seed_to_soil_map, almanac.soil_to_fertilizer_map,
+    almanac.fertilizer_to_water_map, almanac.water_to_light_map,
+    almanac.light_to_temperature_map, almanac.temperature_to_humidity_map,
+    almanac.humidity_to_location_map
+  ]
+
+  assert len(maps) == len(chunks)
+
+  map_lines = []
+  for idx in range(len(chunks)):
+
+    if idx == len(chunks) - 1:
+      # Last item, must read to the end
+      map_lines = lines[chunks[idx] + offset:]
+    else:
+      # Read a chunk
+      map_lines = lines[chunks[idx] + offset:chunks[idx + 1]]
+
+    for line in map_lines:
+      dest_start, source_start, length = [int(x) for x in line.strip().split()]
+      for i in range(length):
+        dest = dest_start + i
+        source = source_start + i
+        maps[idx][source] = dest
+
+  return almanac
 
 
 def main():
@@ -49,16 +86,10 @@ def main():
 
   PrintDebug(f"Seeds: {seeds}")
 
-  chunks = [idx for idx, line in enumerate(lines) if line == ""]
-  PrintDebug(f"Chunks: {chunks}")
+  almanac = BuildAlmanac(lines)
 
-  assert len(chunks) == 7
-
-  # offset is 2 for the whitespace and title for each section
-  offset = 2
-  for line in lines[chunks[1] + 2:chunks[2]]:
-    # seed_to_soil
-    PrintDebug(line)
+  for source in almanac.seed_to_soil_map.keys():
+    PrintDebug(f"{source} -> {almanac.seed_to_soil_map[source]}")
 
 
 if __name__ == '__main__':
