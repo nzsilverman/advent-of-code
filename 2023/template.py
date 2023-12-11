b9cdbd34b25375c2d3efec7e3a47b45f485e97f9
Nathan Silverman <nzsilverman@gmail.com>
2023-12-10 16:08:44 -0800

    Auto format template

--- 2023/template.py
+++ 2023/template.py
@@ -1,23 +1,39 @@
 import sys
 from collections import deque
 
-def PrintRed(skk, end="\n"): PrintDebug("\033[91m{}\033[00m".format(skk), end=end)
-def PrintGreen(skk, end="\n"): PrintDebug("\033[92m{}\033[00m".format(skk), end=end)
-def PrintYellow(skk, end="\n"): PrintDebug("\033[93m{}\033[00m".format(skk), end=end)
-def PrintLightPurple(skk, end="\n"): PrintDebug("\033[94m{}\033[00m".format(skk), end=end)
 
-DEBUG=1
+def PrintRed(skk, end="\n"):
+  PrintDebug("\033[91m{}\033[00m".format(skk), end=end)
+
+
+def PrintGreen(skk, end="\n"):
+  PrintDebug("\033[92m{}\033[00m".format(skk), end=end)
+
+
+def PrintYellow(skk, end="\n"):
+  PrintDebug("\033[93m{}\033[00m".format(skk), end=end)
+
+
+def PrintLightPurple(skk, end="\n"):
+  PrintDebug("\033[94m{}\033[00m".format(skk), end=end)
+
+
+DEBUG = 1
+
+
 def PrintDebug(skk, end="\n"):
-    if DEBUG:
-        print(skk, end=end)
+  if DEBUG:
+    print(skk, end=end)
+
 
 def main():
-    infile = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
-    data = open(infile).read().strip()
-    lines = [line for line in data.split('\n')]
+  infile = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+  data = open(infile).read().strip()
+  lines = [line for line in data.split('\n')]
 
-    for line in lines:
-        PrintDebug(line)
+  for line in lines:
+    PrintDebug(line)
+
 
 if __name__ == '__main__':
-    main()
+  main()
